9f49f2466e60837f51ab8fbd40deeada788b2767
luiz70 <ADG@MacBook-Air-de-Alejandro.local>
2015-07-27 17:24:40 -0500

    files

--- platforms/ios/www/js/controllers/mapa.js
+++ platforms/ios/www/js/controllers/mapa.js
@@ -422,6 +422,7 @@ for(var i=0;i<$scope.points.length;i++)$scope.bnds=$scope.bnds.extend($scope.poi
 }
 $rootScope.buscaEnMap=function(){
 	$scope.resultadoLugares=[];
+            $("#textoBuscado").css("font-size",$("#textoBuscado").css("font-size")+" !important");
 	$("#textoBuscado").val("");
 	$scope.textoBuscado="";
 	$(".buscadorDir").css("opacity",0);
@@ -431,12 +432,16 @@ $rootScope.buscaEnMap=function(){
 	},300,function(){
 		
 	$("#textoBuscado").focus();
-	cordova.plugins.Keyboard.show();
+	if(window.cordova && window.cordova.plugins.Keyboard)cordova.plugins.Keyboard.show();
 	});
+}
+$scope.cierrateclado=function(){
+	if(window.cordova && window.cordova.plugins.Keyboard)cordova.plugins.Keyboard.close();
+	else alert(1);
 }
 $scope.searchEnter=function(event){
 	if(event.keyCode==13){
-       cordova.plugins.Keyboard.close();
+       if(window.cordova && window.cordova.plugins.Keyboard)cordova.plugins.Keyboard.close();
     }
 }
 
@@ -484,7 +489,7 @@ $scope.resultadosBusq=function(results, status) {
 	}
 $rootScope.ocultaBuscador=function(){
 	$scope.textoBuscado="";
-	cordova.plugins.Keyboard.close();
+	if(window.cordova && window.cordova.plugins.Keyboard)cordova.plugins.Keyboard.close();
 	$("#textoBuscado").blur();
 	$("#textoBuscado").val("");
 	$(".buscadorDir")
