64a878492a6b06feeb5a7619d1074822bf8276dc
luiz70 <a00225979@itesm.mx>
2016-01-26 10:09:05 -0600

    asdads

--- www/js/services/radio.js
+++ www/js/services/radio.js
@@ -43,7 +43,7 @@ angular.module('starter.services')
 				$rootScope.eventosMap=[];
 				$rootScope.radio.radio=parseInt(newValue);
 				try{
-				$rootScope.radio.Val=$rootScope.radio.getCircle().getRadius();
+				$rootScope.radio.getCircle().setRadius($rootScope.radio.radio);
 				}catch(err){}
 				//revisaEventos($rootScope.map.ubicacion.position);
 			}
