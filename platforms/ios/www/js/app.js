afdf61202684f2970d812a5983a4a193f14381ec
Mac Sistemas <sistemas@keros.mx>
2016-02-15 12:08:42 -0600

    files

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -7,6 +7,7 @@ angular.module('starter', ['ionic', 'starter.controllers','ionic-material', 'sta
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
+	  
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
